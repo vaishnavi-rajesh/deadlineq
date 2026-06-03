510c30df03e1163ef5cff908f86a4340492bd4a6
vaishnavi-rajesh <vaishnavirajesh361@gmail.com>
2026-06-03 15:01:19 +0530

    added email input test endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app= FastAPI (
     title="DeadlineQ API",
@@ -6,9 +7,21 @@ app= FastAPI (
     version="0.1.0"
 )
 
+class EmailInput(BaseModel):
+    subject:str
+    body:str
+
 @app.get("/")
 def root():
     return{
         "message":"DeadlineQ running successfully",
         "status":"ok"
+    }
+
+@app.post("/extract-test")
+def extract_test(email:EmailInput):
+    return{
+        "received subject":email.subject,
+        "received body":email.body,
+        "message":"email received successfully"
     }
